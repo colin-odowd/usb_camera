5cc49fac6e808135986b22962810786ba4b39f9f
Colin <cdodowdee@gmail.com>
2022-05-20 23:03:54 -0400

    Cleaned up header files

--- Worker1.py
+++ Worker1.py
@@ -7,8 +7,6 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from datetime import datetime
 
-from numpy import True_
-
 class ImagePlayer(QThread):
     def __init__(self):
         super(ImagePlayer, self).__init__()
